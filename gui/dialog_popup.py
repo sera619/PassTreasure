30daa0d2c0f87c4463387b155dab1c33c57fe70f
S3R43o3 <67480273+sera619@users.noreply.github.com>
2025-12-12 22:35:17 +0100

    setup question dialog popup

--- gui/dialog_popup.py
+++ gui/dialog_popup.py
@@ -22,19 +22,29 @@ class DialogPopup(QDialog):
         self.text = text
         self.mode: PopupType = mode
         self._apply_mode()
-        self.play_system_sound()
         self._build_ui()
+        self.play_system_sound()
         self._fill_data()
         
     def _build_ui(self):
         ok_button = self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok)
         cancel_button = self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Cancel)
+        yes_button = self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Yes)
+        no_button = self.ui.buttonBox.button(QDialogButtonBox.StandardButton.No)
         if ok_button:
             ok_button.setStyleSheet(config.Styles.green_button_outlined)
             utils.colorize_icon(ok_button, "check", "green")
+        if yes_button:
+            yes_button.setStyleSheet(config.Styles.green_button_outlined)
+            utils.colorize_icon(yes_button, "check", "green")
+            yes_button.clicked.connect(lambda: self.accept())
         if cancel_button:
             cancel_button.setStyleSheet(config.Styles.red_button_outlined)
             utils.colorize_icon(cancel_button, "close", "red")
+        if no_button:
+            no_button.setStyleSheet(config.Styles.red_button_outlined)
+            utils.colorize_icon(no_button, "close", "red")
+            no_button.clicked.connect(lambda: self.reject())
         self.ui.textLabel.setStyleSheet("font: 10pt Segoe UI; color: #fff;")
         
     def _fill_data(self):
@@ -51,6 +61,8 @@ class DialogPopup(QDialog):
                 winsound.MessageBeep(winsound.MB_ICONHAND)
             case PopupType.WARNING:
                 winsound.MessageBeep(winsound.MB_ICONHAND)
+            case PopupType.QUESTION:
+                winsound.MessageBeep(winsound.MB_OK)
     
     def _apply_mode(self):
         """Sets icon, colors, etc. based on popup mode."""
@@ -66,7 +78,7 @@ class DialogPopup(QDialog):
             case PopupType.INFO:
                 self.setWindowTitle("Information")
                 icon = QPixmap(":/assets/info.png")
-                icon_colored = utils.tint_pixmap(icon, QColor("#6fbdf5"))
+                icon_colored = utils.tint_pixmap(icon, QColor("#66b8f3"))
                 self.ui.iconLabel.setPixmap(icon_colored)
                 self.ui.titleLabel.setStyleSheet("font: bold 15pt Segoe UI; color: #6fbdf5;")
                 self.ui.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
@@ -78,6 +90,7 @@ class DialogPopup(QDialog):
                 self.ui.iconLabel.setPixmap(icon_colored)
                 self.ui.titleLabel.setStyleSheet("font: bold 15pt Segoe UI; color: #f1bd54;")
                 self.ui.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
+                self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setAutoDefault(True)
 
             case PopupType.ERROR:
                 self.setWindowTitle("Error")
@@ -86,4 +99,14 @@ class DialogPopup(QDialog):
                 self.ui.iconLabel.setPixmap(icon_colored)
                 self.ui.titleLabel.setStyleSheet("font: bold 15pt Segoe UI; color: #df2b2b;")
                 self.ui.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
+                self.ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setAutoDefault(True)
 
+
+            case PopupType.QUESTION:
+                self.setWindowTitle("Question")
+                icon = QPixmap(":/assets/question.png")
+                icon_colored = utils.tint_pixmap(icon, QColor("#58d6fc"))
+                self.ui.iconLabel.setPixmap(icon_colored)
+                self.ui.titleLabel.setStyleSheet("font: bold 15pt Segoe UI; color: #58d6fc;")
+                self.ui.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Yes | QDialogButtonBox.StandardButton.No)
+                self.ui.buttonBox.button(QDialogButtonBox.StandardButton.No).setAutoDefault(True)
